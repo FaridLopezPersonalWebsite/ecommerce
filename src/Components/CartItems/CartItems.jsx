edf311df67f5dd4d258f3a13afa166a76ea124a0
Farid Lopez <faridlopez5@gmail.com>
2024-02-17 01:08:33 -0800

    added option to add or remove more of the same item from the cart page

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,4 +1,4 @@
-
+// CartItems.jsx
 import React, { useContext } from 'react';
 import './CartItems.css';
 import remove_icon from '../Assets/cart_cross_icon.png';
@@ -6,7 +6,7 @@ import { ShopContext } from '../../Context/ShopContext';
 import { Link } from 'react-router-dom';
 
 const CartItems = () => {
-  const { getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const { getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className='cartitems'>
@@ -27,10 +27,14 @@ const CartItems = () => {
             <div key={itemId}>
               <div className="cartitems-format cartitems-format-main">
                 <img src={item.image} alt="" className='carticon-product-icon' />
-                <p>{item.title}</p> {/* Change from item.name to item.title */}
-                <p>${item.price}</p> {/* Change from item.new_price to item.price */}
-                <button className='cartitems-quantity'>{quantity}</button>
-                <p>${(item.price * quantity).toFixed(2)}</p> {/* Ensure total is formatted with 2 decimal places */}
+                <p>{item.title}</p>
+                <p>${item.price}</p>
+                <div className='cartitems-quantity-section'>
+                  <button className='cartitems-quantity-btn' onClick={() => removeFromCart(itemId)}>-</button>
+                  <span className='cartitems-quantity'>{quantity}</span>
+                  <button className='cartitems-quantity-btn' onClick={() => addToCart(itemId)}>+</button>
+                </div>
+                <p>${(item.price * quantity).toFixed(2)}</p>
                 <img className='cartitems-remove-icon' src={remove_icon} onClick={() => { removeFromCart(itemId) }} alt="" />
               </div>
               <hr />
